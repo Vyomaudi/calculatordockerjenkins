1c09c3b2c6999e9a85123671b6f71ebace7e4602
Vyomaudi <vyomaudi23@gmail.com>
2020-05-10 19:27:13 +0530

    Final Build==>ADD,SUBTRACT,MULTIPLY,DIVIDE

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,11 +2,24 @@ public class Calculator {
 
 
     public static void main(String[] args) {
-            System.out.println("Add 5 and 6 => = " + add(5, 6));
-
+            System.out.println("Add 5 and 6 => " + add(5, 6));
+            System.out.println("Subtract 20 and 10 ==> " + sub(20,10));
+            System.out.println("Multiply 10 *20 ==> " + mul(10,20));
+            System.out.println("Divide 6 with 2 ==>" + div(6,2));
     }
     static int add(int x,int y){
         return x+y;
     }
-
+    static int sub(int x,int y) { return x-y; }
+    static int mul(int x, int y){ return x*y; }
+    static double div(double x,double y) {
+        double res;
+        if(y==0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        else {
+            res = x/y;
+        }
+        return res;
+    }
 }
--- src/test/java/testclass.java
+++ src/test/java/testclass.java
@@ -14,4 +14,34 @@ public class testclass {
         long result  = cal.add(a,b);
         Assert.assertEquals(expected,result);
     }
+    @Test
+    public void testSub(){
+        int a  = 20;
+        int b = 10;
+        int expected = 10;
+        long result  = cal.sub(a,b);
+        Assert.assertEquals(expected,result);
+    }
+    @Test
+    public void testMul(){
+        int a  = 10;
+        int b = 20;
+        int expected = 200;
+        long result  = cal.mul(a,b);
+        Assert.assertEquals(expected,result);
+    }
+    @Test
+    public void testDiv(){
+        double a  = 22;
+        double b = 5;
+        double expected = 4.4;
+        double result  = cal.div(a,b);
+        Assert.assertEquals(expected,result,0.00005);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        double a = 15;
+        double b = 0;
+        cal.div(a, b);
+    }
 }
